06d8c2bc66c65ea40c23a885ac432e4710f22cfe
Sreenath N <tacticiankerala@gmail.com>
2016-06-28 17:49:16 +0530

    Make clojars as deployment target

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
-(defproject dashboard/lein-template "0.0.1-SNAPSHOT"
+(defproject com.multunus/dashboard-template  "0.0.1-SNAPSHOT"
   :description "Template for dashboard project"
   :url "https://github.com/multunus/dashboard-clj/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
+
   :eval-in-leiningen true)
