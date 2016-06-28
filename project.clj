281c3c5a73a941a9913ef1678731a29fe3718fd5
Sreenath N <tacticiankerala@gmail.com>
2016-06-28 17:15:45 +0530

    Fix description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dashboard/lein-template "0.0.1"
-  :description "Created with lein-create-template"
-  :url "http://example.com/FIXME"
+  :description "Template for dashboard project"
+  :url "https://github.com/multunus/dashboard-clj/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
