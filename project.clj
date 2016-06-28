c7902f406a3d7e40821202151bcd8f3a8ffe6c0e
Sreenath N <tacticiankerala@gmail.com>
2016-06-28 17:36:25 +0530

    Mark template version as snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dashboard/lein-template "0.0.1"
+(defproject dashboard/lein-template "0.0.1-SNAPSHOT"
   :description "Template for dashboard project"
   :url "https://github.com/multunus/dashboard-clj/"
   :license {:name "Eclipse Public License"
